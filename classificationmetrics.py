4e4b75286f42d23da0c39f56ca4e40924ef27dac
aamanrebello <aamanrebello@gmail.com>
2022-04-18 13:24:15 +0100

    Add tests for classification metrics
    Correct some of the metrics too.

--- classificationmetrics.py
+++ classificationmetrics.py
@@ -1,7 +1,27 @@
 import math
 
+# Raises exception if lists have different lengths
+def ensureEqualLength(leftList, rightList):
+    if len(leftList) != len(rightList):
+        raise ValueError('Input lists need to be of same length.')
+
+# Indicator Function
+def indicatorFunction(predictions, true_values, **kwargs):
+    ensureEqualLength(predictions, true_values)
+
+    def pairIndicator(prediction, true):
+        if prediction != 1 and prediction != 0:
+            raise ValueError('The predictions must either be 0 or 1.')
+        if true != 1 and true != 0:
+            raise ValueError('The true values must either be 0 or 1.')
+        return int(prediction != true)
+
+    indicator_losses = list(map(pairIndicator, predictions, true_values))
+    return sum(indicator_losses) / len (indicator_losses)
+
 # Hinge loss
 def hingeLoss(predictions, true_values, **kwargs):
+    ensureEqualLength(predictions, true_values)
     hinge = lambda a, b : max(1 - a*b, 0)
     hinge_losses = list(map(hinge, predictions, true_values))
     return sum(hinge_losses) / len (hinge_losses)
@@ -9,27 +29,31 @@ def hingeLoss(predictions, true_values, **kwargs):
 
 # Binary cross-entropy
 def binaryCrossEntropy(predictions, true_values, **kwargs):
+    ensureEqualLength(predictions, true_values)
 
     def bceTerm(prediction, true):
+        tolerance = 1e-10 #To avoid log(0) terms which are undefined
         if prediction > 1 or true > 1:
             raise ValueError('The prediction and true values all need to be between 0 and 1.')
         if prediction < 0 or true < 0:
             raise ValueError('The prediction and true values all need to be between 0 and 1.')
-        return -(true*math.log(prediction) + (1 - true)*math.log(1 - prediction))
+        return -(true*math.log(prediction + tolerance) + (1 - true)*math.log(1 - prediction + tolerance))
 
     bce_losses = list(map(bceTerm, predictions, true_values))
     return sum(bce_losses) / len(bce_losses)
 
 
-# Binary cross-entropy
+# Kullback-Leibler divergence
 def KullbackLeiblerDivergence(predictions, true_values, **kwargs):
+    ensureEqualLength(predictions, true_values)
 
     def KLTerm(prediction, true):
+        tolerance = 1e-10 #To avoid log(0) terms which are undefined
         if prediction > 1 or true > 1:
             raise ValueError('The prediction and true values all need to be between 0 and 1.')
         if prediction < 0 or true < 0:
             raise ValueError('The prediction and true values all need to be between 0 and 1.')
-        return true*math.log(true/prediction)
+        return true*math.log(true+tolerance) - true*math.log(prediction+tolerance)
 
     KL_losses = list(map(KLTerm, predictions, true_values))
     return sum(KL_losses) / len(KL_losses)
--- classificationmetrics_test.py
+++ classificationmetrics_test.py
@@ -1,11 +1,149 @@
 from math import log
 import unittest
+from classificationmetrics import ensureEqualLength, indicatorFunction, hingeLoss, binaryCrossEntropy, KullbackLeiblerDivergence
+
+class TestEnsureEqualLength(unittest.TestCase):
+
+    def test_unequal_length(self):
+        l1 = [1,0,1,1]
+        l2 = [0,1]
+        test = False
+        try:
+            result = ensureEqualLength(l1, l2)
+        except ValueError as inst:
+            self.assertEqual(inst.args[0], 'Input lists need to be of same length.')
+            test = True
+        self.assertTrue(test)
+
+    def test_equal_length(self):
+        l1 = [1,0,1,1]
+        l2 = [0,1,0,1]
+        test = False
+        try:
+            result = ensureEqualLength(l1, l2)
+        except ValueError as inst:
+            self.assertEqual(inst.args[0], 'Input lists need to be of same length.')
+            test = True
+        self.assertFalse(test)
+
+class TestIndicator(unittest.TestCase):
+
+    def test_fractional_prediction(self):
+        l1 = [1,0,0.9,1]
+        l2 = [0,1,0,1]
+        test = False
+        try:
+            result = indicatorFunction(l1, l2)
+        except ValueError as inst:
+            self.assertEqual(inst.args[0], 'The predictions must either be 0 or 1.')
+            test = True
+        self.assertTrue(test)
+
+    def test_large_true_value(self):
+        l1 = [1,0,0,1]
+        l2 = [0,1,0,11]
+        test = False
+        try:
+            result = indicatorFunction(l1, l2)
+        except ValueError as inst:
+            self.assertEqual(inst.args[0], 'The true values must either be 0 or 1.')
+            test = True
+        self.assertTrue(test)
+
+    def test_prediction(self):
+        l1 = [1,0,0,1,0,1,0,0,1,1]
+        l2 = [0,1,0,1,0,0,0,0,0,1]
+        expected_result = 0.4
+        result = indicatorFunction(l1, l2)
+        self.assertAlmostEqual(expected_result, result)
+
 
 class TestHingeLoss(unittest.TestCase):
-    pass
+
+    def test_prediction(self):
+        l1 = [0.1,0,1,0.5,10,1,0.2,0,1.8,1]
+        l2 = [0.1,1,1,0.7,1,0.9,0.2,0,0.1,1.2]
+        expected_result = (1 - 0.01 + 1 + 0 + 1 - 0.35 + 0 + 1 - 0.9 + 1 - 0.04 + 1 + 1 - 0.18 + 0)/10
+        result = hingeLoss(l1, l2)
+        self.assertAlmostEqual(expected_result, result)
 
 class TestBinaryCrossEntropy(unittest.TestCase):
-    pass
+
+    def test_negative_prediction(self):
+        l1 = [0.1,0,1,-0.5,1.0]
+        l2 = [0.1,1,1,0.7,1]
+        test = False
+        try:
+            result = binaryCrossEntropy(l1, l2)
+        except ValueError as inst:
+            self.assertEqual(inst.args[0], 'The prediction and true values all need to be between 0 and 1.')
+            test = True
+        self.assertTrue(test)
+
+    def test_large_true_value(self):
+        l1 = [0.1,0,1,0.5,1.0]
+        l2 = [0.1,1,1,7,1]
+        test = False
+        try:
+            result = binaryCrossEntropy(l1, l2)
+        except ValueError as inst:
+            self.assertEqual(inst.args[0], 'The prediction and true values all need to be between 0 and 1.')
+            test = True
+        self.assertTrue(test)
+
+    def test_prediction(self):
+        l1 = [0.1, 0.1, 0.9, 0.5, 0.9, 0.45, 0.2, 0.1, 0.8, 0.11]
+        l2 = [0,   1,   1,   0.7, 1,   0.9,  0,   0,   0.1, 0.2]
+        expected_result = (0.105360516 + 2.30258509 + 0.105360516 + 0.693147181 + 0.105360516 + 0.778440627 + 0.223143551 + 0.105360516 + 1.47080848 + 0.534682036)/10
+        result = binaryCrossEntropy(l1, l2)
+        self.assertAlmostEqual(expected_result, result)
+
+    def test_zeroes_and_ones(self):
+        l1 = [0,   0.1, 0, 0.5, 1, 0.45, 0.2,   1,   0.8, 0.11]
+        l2 = [0,   1,   1, 0.7, 1,   0.9,  0,     0,   0.1, 0.2]
+        expected_result = (0 + 2.30258509 + 23.0258509 + 0.693147181 + 0 + 0.778440627 + 0.223143551 + 23.0258509 + 1.47080848 + 0.534682036)/10
+        result = binaryCrossEntropy(l1, l2)
+        self.assertAlmostEqual(expected_result, result)
 
 class TestKullbackLeiblerDivergence(unittest.TestCase):
-    pass
+
+    def test_negative_true_value(self):
+        l1 = [0.1,1,1,0.7,1]
+        l2 = [0.1,0,1,-0.1,1.0]
+        test = False
+        try:
+            result = KullbackLeiblerDivergence(l1, l2)
+        except ValueError as inst:
+            self.assertEqual(inst.args[0], 'The prediction and true values all need to be between 0 and 1.')
+            test = True
+        self.assertTrue(test)
+
+    def test_large_prediction(self):
+        l1 = [0.1,1,1,1.1,1]
+        l2 = [0.1,0,1,0.5,1.0]
+        test = False
+        try:
+            result = KullbackLeiblerDivergence(l1, l2)
+        except ValueError as inst:
+            self.assertEqual(inst.args[0], 'The prediction and true values all need to be between 0 and 1.')
+            test = True
+        self.assertTrue(test)
+
+    def test_prediction(self):
+        l1 = [0.1, 0.1, 0.9, 0.5, 0.9, 0.45, 0.2, 0.1, 0.8, 0.11]
+        l2 = [0,   1,   1,   0.7, 1,   0.9,  0,   0,   0.1, 0.2]
+        expected_result = (0.105360516 + 2.30258509 + 0.105360516 + 0.693147181 + 0.105360516 + 0.778440627 + 0.223143551 + 0.105360516 + 1.47080848 + 0.534682036)/10
+        result = KullbackLeiblerDivergence(l1, l2)
+        self.assertAlmostEqual(expected_result, result)
+
+    def test_zeroes_and_ones(self):
+        l1 = [0,   0.1, 0, 0.5, 1, 0.45, 0.2,   1,   0.8, 0.11]
+        l2 = [0,   1,   1, 0.7, 1,   0.9,  0,     0,   0.1, 0.2]
+        expected_result = (0 + 2.40794561 + 23.0258509 + 0.970406053 + 0 + 0.778440627 + 0.223143551 + 23.0258509 + 1.47080848 + 0.534682036)/10
+        result = KullbackLeiblerDivergence(l1, l2)
+        self.assertAlmostEqual(expected_result, result)
+
+
+if __name__ == '__main__':
+    # Run tests
+    unittest.main()
