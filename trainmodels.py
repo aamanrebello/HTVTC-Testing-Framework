a38dd6bb016a830747106843bc95037994ed9e7f
aamanrebello <56508438+aamanrebello@users.noreply.github.com>
2022-04-21 14:05:27 +0530

    Progress on model-training framework
    Enable K-NN, random forest, check on the ML task, a first example of a workflow at line 96.

--- trainmodels.py
+++ trainmodels.py
@@ -1,49 +1,100 @@
 from loaddata import loadData, trainTestSplit
+import regressionmetrics
+import classificationmetrics
 
 # Returns a function 'evaluate' that accepts hyperparameters for the specified
 # machine learning algorithm and evaluates a model trained with these hyperparameters
 # on the validation dataset
-def evaluationFunctionGenerator(algorithm = 'ridgeregression', data):
+def evaluationFunctionGenerator(data, algorithm = 'svm-rbf', task='classification'):
     train_X = data['training_features']
     train_y = data['training_labels']
     validation_X = data['validation_features']
     validation_y = data['validation_labels']
 
-    if algorithm == 'ridgeregression':
-        def evaluate(alpha, metric):
+    # Ridge regression (1 hyperparameter)
+    if algorithm == 'ridgeregression' and task=='regression':
+        def evaluate(alpha, metric, **kwargs):
             from sklearn.linear_model import Ridge
             clf = Ridge(alpha = alpha)
             clf.fit(train_X, train_y)
             validation_predictions = clf.predict(validation_X)
-            return metric(validation_y, validation_predictions)
+            return metric(validation_y, validation_predictions, **kwargs)
         return evaluate
 
-    elif algorithm == 'svm-rbf':
-        def evaluate(C, gamma, metric):
+    # SVM using radial basis function kernel (2 hyperparameters)
+    elif algorithm == 'svm-rbf' and task=='classification':
+        def evaluate(C, gamma, metric, **kwargs):
             from sklearn import svm
             clf = svm.SVC(C = C, kernel = 'rbf', gamma = gamma)
             clf.fit(train_X, train_y)
             validation_predictions = clf.predict(validation_X)
-            return metric(validation_y, validation_predictions)
+            return metric(validation_y, validation_predictions, **kwargs)
         return evaluate
 
-    elif algorithm == 'svm-polynomial':
-        def evaluate(C, gamma, constant_term, degree, metric):
+    # SVM using polynomial kernel (4 hyperparameters)
+    elif algorithm == 'svm-polynomial' and task=='classification':
+        def evaluate(C, gamma, constant_term, degree, metric, **kwargs):
             from sklearn import svm
             clf = svm.SVC(C = C, kernel = 'poly', gamma = gamma, degree = degree, coef0 = constant_term)
             clf.fit(train_X, train_y)
             validation_predictions = clf.predict(validation_X)
-            return metric(validation_y, validation_predictions)
+            return metric(validation_y, validation_predictions, **kwargs)
         return evaluate
 
-    elif algorithm == 'knn-regression':
-        pass
+    # K-nearest neighbour regression (3 hyperparameters)    
+    elif algorithm == 'knn-regression' and task == 'regression':
+        def evaluate(N, weightingFunction, distanceFunction, metric, **kwargs):
+            from sklearn.neighbors import KNeighborsRegressor
+            clf = None
+            if distanceFunction == 'minkowski': # Stands for generalised Minkowski distance
+                p = None
+                if 'p' not in kwargs.keys():
+                    p = 2 # Use Euclidean distance by default
+                else:
+                    p = kwargs['p'] # Use provided value of p
+                clf = KNeighborsRegressor(n_neighbours=N, weights=weightingFunction, p=p)
+            else:
+                clf = KNeighborsRegressor(n_neighbours=N, weights=weightingFunction, metric=distanceFunction)
+            clf.fit(train_X, train_y)
+            validation_predictions = clf.predict(validation_X)
+            return metric(validation_y, validation_predictions, **kwargs)
+        return evaluate
 
-    elif algorithm == 'knn-classification':
-        pass
+    # K-nearest neighbour classification (3 hyperparameters)
+    elif algorithm == 'knn-classification' and task=='classification':
+        def evaluate(N, weightingFunction, distanceFunction, metric, **kwargs):
+            from sklearn.neighbors import KNeighborsClassifier
+            clf = None
+            if distanceFunction == 'minkowski': # Stands for generalised Minkowski distance
+                p = None
+                if 'p' not in kwargs.keys():
+                    p = 2 # Use Euclidean distance by default
+                else:
+                    p = kwargs['p'] # Use provided value of p
+                clf = KNeighborsClassifier(n_neighbours=N, weights=weightingFunction, p=p)
+            else:
+                clf = KNeighborsClassifier(n_neighbours=N, weights=weightingFunction, metric=distanceFunction)
+            clf.fit(train_X, train_y)
+            validation_predictions = clf.predict(validation_X)
+            return metric(validation_y, validation_predictions, **kwargs)
+        return evaluate
 
-    elif algorithm == 'random-forest':
-        pass
+    # Random forest classification (6 hyperparameters)
+    elif algorithm == 'random-forest' and task=='classification':
+        def evaluate(no_trees, max_tree_depth, bootstrap, min_samples_split, no_features, metric, **kwargs):
+            from sklearn.ensemble import RandomForestClassifier
+            clf = RandomForestClassifier(n_estimators=no_trees, max_depth=max_tree_depth, bootstrap=bootstrap, min_samples_split=min_samples_split, max_features=no_features, random_state=0)
+            clf.fit(train_X, train_y)
+            validation_predictions = clf.predict(validation_X)
+            return metric(validation_y, validation_predictions, **kwargs)
+        return evaluate
 
-    else
+    else:
         raise ValueError('The algorithm specified is not recognised.')
+
+
+#Let's test the function
+task = 'regression'
+data = loadData(source='sklearn', identifier='diabetes', task=task)
+data_split = trainTestSplit(data)
+func = evaluationFunctionGenerator(data_split, algorithm = 'ridgeregression', task=task)
