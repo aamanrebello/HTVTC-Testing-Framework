5fd3687bd27f35ba68363d643c2045f7e75cb6ae
aamanrebello <56508438+aamanrebello@users.noreply.github.com>
2022-04-25 12:17:47 +0530

    Support for value list, evaluation mode
    Specify the range of hyperparameter values as a list of values, and also specify the evaluation mode.

--- generateerrortensor.py
+++ generateerrortensor.py
@@ -6,17 +6,27 @@ import itertools
 #to results of the evaluation function, evaluated on hyperparameters corresponding to the nonzero position
 def generateIncompleteErrorTensor(eval_func, ranges_dict, known_fraction, metric, eval_trials=5, **kwargs):
 
+    evaluation_mode = 'prediction'
+    if 'evaluation_mode' in kwargs.keys():
+        evaluation_mode = kwargs['evaluation_mode']
+
     hyperparameter_values = {}
     tensor_dimensions_list = []
     tensor_elements = 1
 
-    # Obtain the full lists of values for each hyperparameter from the provided ranges
+    # Obtain the full lists of values for each hyperparameter from the provided ranges/provided list of values
     for key in ranges_dict.keys():
         info = ranges_dict[key]
-        start = float(info['start'])
-        end = float(info['end'])
-        interval = float(info['interval'])
-        value_list = np.linspace(start, end, int((end-start)/interval)+1)
+        value_list = None
+        #If the values are already provided as a list
+        if 'values' in info.keys():
+            value_list = info['values']
+        #If the values are provided as a range
+        else:
+            start = float(info['start'])
+            end = float(info['end'])
+            interval = float(info['interval'])
+            value_list = np.linspace(start, end, int(round((end-start)/interval, 0))+1)
         # Update outer values
         hyperparameter_values[key] = value_list
         tensor_dimensions_list.append(len(value_list))
@@ -34,7 +44,7 @@ def generateIncompleteErrorTensor(eval_func, ranges_dict, known_fraction, metric
 
     # Randomly sample from all_indices
     sampled_indices = random.sample(all_indices, known_elements)
-    
+
     # Populate the empty tensor
     for tensor_index in sampled_indices:
         current_hyperparameter_values = {}
@@ -48,7 +58,7 @@ def generateIncompleteErrorTensor(eval_func, ranges_dict, known_fraction, metric
         # Assign to tensor using result of eval_func averaged over multiple trials
         eval_result_avg = 0
         for trial in range(eval_trials):
-            eval_result_avg += eval_func(**current_hyperparameter_values, metric=metric)/eval_trials
+            eval_result_avg += eval_func(**current_hyperparameter_values, metric=metric, evaluation_mode=evaluation_mode)/eval_trials
         error_tensor[tuple(tensor_index)] = eval_result_avg
 
     #return populated tensor
