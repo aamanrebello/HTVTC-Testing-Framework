65e1de61a04cac521ca1c6226b75a36f6db66220
aamanrebello <56508438+aamanrebello@users.noreply.github.com>
2022-04-26 13:37:35 +0530

    Function to convert indices to hyperparameters
    Uses a range dictionary that specifies the hyperparameter value ranges.

--- tensorsearch.py
+++ tensorsearch.py
@@ -1,7 +1,7 @@
 import tensorly as tl
 import numpy as np
 
-# Converts a 1D index to a higher dimensional index
+#Converts a 1D index to a higher dimensional index-----------------------------------------------------
 def higherDimensionalIndex(index, dimensions):
     dimension_list = [d for d in dimensions] # Convert dimensions tuple into list
     no_dimensions = len(dimension_list) # Find number of dimensions
@@ -12,8 +12,10 @@ def higherDimensionalIndex(index, dimensions):
         index = index // dimension_list[i]
     # Convert to tuple
     return tuple(higher_dimensional_index_list)
+#-----------------------------------------------------------------------------------------------------
 
-# Function
+
+#Function to find smallest or largest elements in tensor along with indices---------------------------
 def findBestValues(tensor, smallest=True, number_of_values=1):
     tensor_dimensions = tl.shape(tensor) # Obtain tensor dimensions
     vector = tensor.flatten(order='C') # flatten tensor along increasing order of dimensions
@@ -29,3 +31,33 @@ def findBestValues(tensor, smallest=True, number_of_values=1):
         'values': values,
         'indices': higher_dimensional_indices
         }
+#-----------------------------------------------------------------------------------------------------
+
+
+#Given a list of indices, return the corresponding hyperparameters in the tensor representation-------
+def hyperparametersFromIndices(index_list, hyperparameter_ranges_dict):
+    # Throw exception if number of indices is unequal to the number of hyperparameters
+    if len(index_list[0]) != len(hyperparameter_ranges_dict.keys()):
+        raise ValueError(f'The indices ({len(index_list[0])}) and hyperparameter configuration ({len(hyperparameter_ranges_dict.keys())}) have unequal dimensions.')
+    hyperparameter_values = []
+    for index in index_list:
+        dimension_index = 0
+        current_hyperparameter_values = {}
+        # Obtain the full lists of values for each hyperparameter from the provided ranges/provided list of values
+        for key in hyperparameter_ranges_dict.keys():
+            info = hyperparameter_ranges_dict[key]
+            value_list_index = int(index[dimension_index])
+            #If the values are already provided as a list
+            if 'values' in info.keys():
+                value_list = info['values']
+                current_hyperparameter_values[key] = value_list[value_list_index]
+            #If the values are provided as a range, no need to create a value list
+            else:
+                start = float(info['start'])
+                interval = float(info['interval'])
+                current_hyperparameter_values[key] = start + value_list_index*interval
+            dimension_index += 1
+        # Update outer values
+        hyperparameter_values.append(current_hyperparameter_values)
+    return hyperparameter_values
+#-----------------------------------------------------------------------------------------------------
