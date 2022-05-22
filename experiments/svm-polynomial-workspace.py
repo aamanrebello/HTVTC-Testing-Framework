a8eacdcf0fb9b5920769be449f9c5258e10d6c05
aamanrebello <aamanrebello@gmail.com>
2022-05-22 22:39:18 +0100

    Tensor generation, svm-polynomial
    Using the iris dataset for classification

--- experiments/svm-polynomial-workspace.py
+++ experiments/svm-polynomial-workspace.py
@@ -16,7 +16,7 @@ import classificationmetrics
 
 #OVERALL CONFIGURATION================================
 BASE_PATH = 'saved-arrays/svm-polynomial/'
-FILE_NAME = 'iris-hinge-30-1.npy'
+FILE_NAME = 'iris-hinge-30-2.npy'
 PATH = BASE_PATH + FILE_NAME
 load_tensor = False
 
@@ -27,36 +27,36 @@ if load_tensor:
     tensor = np.load(PATH)
 else:
     task = 'classification'
-        data = loadData(source='sklearn', identifier='iris', task=task)
-        binary_data = extractZeroOneClasses(data)
-        adjusted_data = convertZeroOne(binary_data, -1, 1)
-        data_split = trainTestSplit(adjusted_data)
-        func = evaluationFunctionGenerator(data_split, algorithm='svm-polynomial', task=task)
+    data = loadData(source='sklearn', identifier='iris', task=task)
+    binary_data = extractZeroOneClasses(data)
+    adjusted_data = convertZeroOne(binary_data, -1, 1)
+    data_split = trainTestSplit(adjusted_data)
+    func = evaluationFunctionGenerator(data_split, algorithm='svm-polynomial', task=task)
 
-        ranges_dict = {
-            'C': {
-                'start': 0.1,
-                'end': 3.0,
-                'interval': 0.1,
-            },
-            'gamma': {
-                'start': 0.1,
-                'end': 3.0,
-                'interval': 0.1,
-            },
-            'constant_term': {
-                'start': 0.0,
-                'end': 3.0,
-                'interval': 0.1,
-            },
-            'degree': {
-                'start': 0.0,
-                'end': 3.0,
-                'interval': 1.0,
-            }
+    ranges_dict = {
+        'C': {
+            'start': 0.1,
+            'end': 3.0,
+            'interval': 0.1,
+        },
+        'gamma': {
+            'start': 0.1,
+            'end': 3.0,
+            'interval': 0.1,
+        },
+        'constant_term': {
+            'start': 0.0,
+            'end': 3.0,
+            'interval': 0.1,
+        },
+        'degree': {
+            'start': 0.0,
+            'end': 3.0,
+            'interval': 1.0,
         }
+    }
         
-        result = generateIncompleteErrorTensor(func, ranges_dict, 1.0, metric=classificationmetrics.hingeLoss, evaluation_mode='raw-score')
-        np.save(file=PATH, arr=tensor)
+    tensor = generateIncompleteErrorTensor(func, ranges_dict, 1.0, metric=classificationmetrics.hingeLoss, evaluation_mode='raw-score')
+    np.save(file=PATH, arr=tensor)
 
 print(f'STAGE 1 - TENSOR GENERATED - shape: {tensor.shape}')
