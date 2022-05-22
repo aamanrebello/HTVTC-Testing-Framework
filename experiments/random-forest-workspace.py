ad8bda476680d069f964014a6654a5fdff8a9297
aamanrebello <aamanrebello@gmail.com>
2022-05-22 22:43:44 +0100

    Tensor generation, random forest
    Wine dataset for classification

--- experiments/random-forest-workspace.py
+++ experiments/random-forest-workspace.py
@@ -16,9 +16,9 @@ import classificationmetrics
 
 #OVERALL CONFIGURATION================================
 BASE_PATH = 'saved-arrays/random-forest/'
-FILE_NAME = 'breast-cancer-indicator-100-1.npy'
+FILE_NAME = 'wine-probability-KLD-5-2.npy'
 PATH = BASE_PATH + FILE_NAME
-load_tensor = False
+load_tensor = True
 
 #OBTAIN TENSOR========================================
 tensor = None
@@ -27,24 +27,35 @@ if load_tensor:
     tensor = np.load(PATH)
 else:
     task = 'classification'
-    data = loadData(source='sklearn', identifier='breast_cancer', task=task)
-    data_split = trainTestSplit(data)
-    func = evaluationFunctionGenerator(data_split, algorithm='svm-rbf', task=task)
+    data = loadData(source='sklearn', identifier='wine', task=task)
+    binary_data = extractZeroOneClasses(data)
+    data_split = trainTestSplit(binary_data)
+    func = evaluationFunctionGenerator(data_split, algorithm='random-forest', task=task)
 
     ranges_dict = {
-        'C': {
-            'start': 0.05,
-            'end': 5.00,
-            'interval': 0.05,
-            },
-        'gamma': {
-            'start': 0.05,
-            'end': 5.00,
-            'interval': 0.05,
-            }
-        }
+        'no_trees': {
+            'values':[1,10,20,30,40]
+        },
+        'max_tree_depth': {
+            'values':[1, 5, 10, 15, 20]
+        },
+        'bootstrap': {
+            'values': [True, False]
+        },
+        'min_samples_split': {
+            'start': 2.0,
+            'end': 10.0,
+            'interval': 1.0,
+        },
+        'no_features': {
+            'start': 1.0,
+            'end': 10.0,
+            'interval': 1.0,
+        },
+    }
         
-    tensor = generateIncompleteErrorTensor(func, ranges_dict, 1.0, metric=classificationmetrics.indicatorFunction)
+    tensor = generateIncompleteErrorTensor(func, ranges_dict, 1.0, metric=classificationmetrics.KullbackLeiblerDivergence, evaluation_mode='probability')
     np.save(file=PATH, arr=tensor)
 
 print(f'STAGE 1 - TENSOR GENERATED - shape: {tensor.shape}')
+print(tensor)
