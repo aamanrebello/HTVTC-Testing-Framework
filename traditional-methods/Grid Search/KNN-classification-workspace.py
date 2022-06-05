03771d6ff4c91b5695c4a527b1458a1dbec4ca85
aamanrebello <aamanrebello@gmail.com>
2022-06-05 15:01:52 +0100

    Update grid search workspaces

--- traditional-methods/Grid Search/KNN-classification-workspace.py
+++ traditional-methods/Grid Search/KNN-classification-workspace.py
@@ -1,12 +1,47 @@
+#Enable importing code from parent directory
+import os, sys
+current_path = os.getcwd()
+parent = os.path.dirname(current_path)
+sys.path.insert(1, parent)
+parent_of_parent = os.path.dirname(parent)
+sys.path.insert(1, parent_of_parent)
+
 import optuna
+from commonfunctions import generate_range
+from trainmodels import evaluationFunctionGenerator
+from loaddata import loadData, trainTestSplit, extractZeroOneClasses, convertZeroOne
+import regressionmetrics
+import classificationmetrics
+import time
+
+#Library only applicable in linux
+#from resource import getrusage, RUSAGE_SELF
+
+task = 'classification'
+data = loadData(source='sklearn', identifier='wine', task=task)
+binary_data = extractZeroOneClasses(data)
+data_split = trainTestSplit(binary_data)
+func = evaluationFunctionGenerator(data_split, algorithm='knn-classification', task=task)
 
 
 def objective(trial):
-    x = trial.suggest_float("x", -100, 100)
-    y = trial.suggest_int("y", -100, 100)
-    return x ** 2 + y ** 2
+    N = trial.suggest_int("N", 1, 101, step=1)
+    p = trial.suggest_int("p", 1, 101, step=1)
+    weightingFunction = trial.suggest_categorical("weightingFunction", ['uniform', 'distance'])
+    distanceFunction = trial.suggest_categorical("distanceFunction", ['minkowski'])
+    
+    return func(N=N, weightingFunction=weightingFunction, distanceFunction=distanceFunction, p=p, metric=classificationmetrics.indicatorFunction)
 
+start_time = time.perf_counter()
 
-search_space = {"x": [-50, 0, 50], "y": [-99, 0, 99]}
+N_search_range = generate_range(1, 100, 1)
+p_search_range = generate_range(1, 100, 1)
+search_space = {"N": N_search_range, "weightingFunction": ['uniform', 'distance'], "distanceFunction": ['minkowski'], "p": p_search_range}
 study = optuna.create_study(sampler=optuna.samplers.GridSampler(search_space))
-study.optimize(objective)
+study.optimize(objective, timeout=600)
+#resource_usage = getrusage(RUSAGE_SELF)
+end_time = time.perf_counter()
+print('\n\n\n')
+print(f'Execution time: {end_time - start_time}')
+#print(f'Resource usage: {resource_usage}')
+
--- traditional-methods/Grid Search/random-forest-workspace.py
+++ traditional-methods/Grid Search/random-forest-workspace.py
@@ -1,12 +1,47 @@
+#Enable importing code from parent directory
+import os, sys
+current_path = os.getcwd()
+parent = os.path.dirname(current_path)
+sys.path.insert(1, parent)
+parent_of_parent = os.path.dirname(parent)
+sys.path.insert(1, parent_of_parent)
+
 import optuna
+from commonfunctions import generate_range
+from trainmodels import evaluationFunctionGenerator
+from loaddata import loadData, trainTestSplit, extractZeroOneClasses, convertZeroOne
+import regressionmetrics
+import classificationmetrics
+import time
+
+#Library only applicable in linux
+#from resource import getrusage, RUSAGE_SELF
+
+task = 'classification'
+data = loadData(source='sklearn', identifier='wine', task=task)
+binary_data = extractZeroOneClasses(data)
+data_split = trainTestSplit(binary_data)
+func = evaluationFunctionGenerator(data_split, algorithm='random-forest', task=task)
 
 
 def objective(trial):
-    x = trial.suggest_float("x", -100, 100)
-    y = trial.suggest_int("y", -100, 100)
-    return x ** 2 + y ** 2
+    no_trees = trial.suggest_categorical("no_trees", [1,10,20,30,40])
+    max_tree_depth = trial.suggest_categorical("max_tree_depth", [1, 5, 10, 15, 20])
+    bootstrap = trial.suggest_categorical("bootstrap", [True, False])
+    min_samples_split = trial.suggest_int("min_samples_split", 2, 11, step=1)
+    no_features = trial.suggest_int("no_features", 1, 11, step=1)
+    
+    return func(no_trees=no_trees, max_tree_depth=max_tree_depth, bootstrap=bootstrap, min_samples_split=min_samples_split, no_features=no_features, metric=classificationmetrics.KullbackLeiblerDivergence)
 
+start_time = time.perf_counter()
 
-search_space = {"x": [-50, 0, 50], "y": [-99, 0, 99]}
+min_samples_split_search_range = generate_range(2.0, 10.0, 1.0)
+no_features_search_range = generate_range(1.0, 10.0, 1.0)
+search_space = {"no_trees": [1,10,20,30,40], "max_tree_depth": [1, 5, 10, 15, 20], "bootstrap": [True, False], "min_samples_split": min_samples_split_search_range, "no_features": no_features_search_range}
 study = optuna.create_study(sampler=optuna.samplers.GridSampler(search_space))
-study.optimize(objective)
+study.optimize(objective, timeout=600)
+#resource_usage = getrusage(RUSAGE_SELF)
+end_time = time.perf_counter()
+print('\n\n\n')
+print(f'Execution time: {end_time - start_time}')
+#print(f'Resource usage: {resource_usage}')
