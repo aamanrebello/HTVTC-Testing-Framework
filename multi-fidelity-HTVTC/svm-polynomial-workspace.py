9f43009233372fc1a504584ec3ed091824449e2a
aamanrebello <56508438+aamanrebello@users.noreply.github.com>
2022-07-18 00:04:13 +0100

    Saving cross-validation array (svm-polynomial)

--- multi-fidelity-HTVTC/svm-polynomial-workspace.py
+++ multi-fidelity-HTVTC/svm-polynomial-workspace.py
@@ -11,7 +11,7 @@ p2 = os.path.abspath('../experiments')
 sys.path.insert(1, p2)
 
 from generateerrortensor import generateIncompleteErrorTensor
-from trainmodels import evaluationFunctionGenerator
+from trainmodels import evaluationFunctionGenerator, crossValidationFunctionGenerator
 from loaddata import loadData, trainTestSplit, extractZeroOneClasses, convertZeroOne
 from commonfunctions import Hamming_distance, norm_difference, sortedBestValues, common_count
 from tensorcompletion import tensorcomplete_CP_WOPT_dense, tensorcomplete_TKD_Geng_Miles, tensorcomplete_TMac_TT
@@ -21,34 +21,73 @@ import classificationmetrics
 
 
 #OVERALL CONFIGURATION================================
-BASE_PATH = '../experiments/saved-arrays/svm-polynomial/'
+BASE_PATH = 'saved-cross-validation-arrays/svm-polynomial/'
 FILE_NAME = 'iris-hinge-30-1'
 ARR_EXTN = '.npy'
 ARR_PATH = BASE_PATH + FILE_NAME + ARR_EXTN
 RANGE_DICT_EXTN = '.json'
 RANGE_DICT_PATH = BASE_PATH + FILE_NAME + '-ranges' + RANGE_DICT_EXTN
+load_tensor = True
 
 #LOAD COMPLETE TENSOR FROM FILE=======================
-tensor = np.load(ARR_PATH)
+tensor = None
 ranges_dict = None
-with open(RANGE_DICT_PATH, 'r') as fp:
-    ranges_dict = json.load(fp)
 
 task = 'classification'
 data = loadData(source='sklearn', identifier='iris', task=task)
 binary_data = extractZeroOneClasses(data)
 adjusted_data = convertZeroOne(binary_data, -1, 1)
-data_split = trainTestSplit(adjusted_data)
+data_split = trainTestSplit(adjusted_data, method = 'cross_validation')
 
-budget_type = 'samples'
-budget_fraction = 0.25
-func = evaluationFunctionGenerator(data_split, algorithm='svm-polynomial', task=task, budget_type=budget_type, budget_fraction=budget_fraction)
+if load_tensor:
+    tensor = np.load(ARR_PATH)
+    with open(RANGE_DICT_PATH, 'r') as fp:
+        ranges_dict = json.load(fp)
+else:
+    nobudgetfunc = crossValidationFunctionGenerator(data_split, algorithm='svm-polynomial', task=task)
+
+    ranges_dict = {
+        'C': {
+            'start': 0.1,
+            'end': 3.0,
+            'interval': 0.1,
+        },
+        'gamma': {
+            'start': 0.1,
+            'end': 3.0,
+            'interval': 0.1,
+        },
+        'constant_term': {
+            'start': 0.0,
+            'end': 3.0,
+            'interval': 0.1,
+        },
+        'degree': {
+            'start': 0.0,
+            'end': 3.0,
+            'interval': 1.0,
+        }
+    }
+    with open(RANGE_DICT_PATH, 'w') as fp:
+        json.dump(ranges_dict , fp)
+
+    #Note that while the function says generate incomplete, we are actually generating a complete tensor
+    #as the fraction is 1.0
+    tensor, _ = generateIncompleteErrorTensor(nobudgetfunc, ranges_dict, 1.0, metric=classificationmetrics.hingeLoss, evaluation_mode='raw-score', eval_trials=1)
+    np.save(file=ARR_PATH, arr=tensor)
+
+print(f'STAGE 1 - COMPLETE TENSOR GENERATED')
+
 
 #GENERATE INCOMPLETE TENSOR===========================
-known_fraction = 0.25
-incomplete_tensor, known_indices = generateIncompleteErrorTensor(func, ranges_dict, known_fraction, metric=classificationmetrics.hingeLoss, evaluation_mode='raw-score')
+budget_type = 'samples'
+budget_fraction = 0.65
+budgetfunc = crossValidationFunctionGenerator(data_split, algorithm='svm-polynomial', task=task, budget_type=budget_type, budget_fraction=budget_fraction)
 
-print(f'STAGE 1 - INCOMPLETE AND COMPLETE TENSOR GENERATED')
+known_fraction = 0.25
+incomplete_tensor, known_indices = generateIncompleteErrorTensor(budgetfunc, ranges_dict, known_fraction, metric=classificationmetrics.hingeLoss, evaluation_mode='raw-score', eval_trials=1)
+
+print(f'STAGE 2 - INCOMPLETE TENSOR GENERATED')
 
 #OBTAIN BEST HYPERPARAMETER COMBINATIONS=============
 smallest = True
@@ -63,20 +102,26 @@ no_elements_1pc = int(0.01*(tensor.size))
 sorted_dict_1pc = sortedBestValues(tensor, smallest=smallest, number_of_values=no_elements_1pc)
 #The top 20
 sorted_dict_top20 = sortedBestValues(tensor, smallest=smallest, number_of_values=20)
-print(f'STAGE 2 - TRUE BEST COMBINATIONS IDENTIFIED')
+print(f'STAGE 3 - TRUE BEST COMBINATIONS IDENTIFIED')
 
 #TEST TENSOR COMPLETION================================
 tensor_norm = np.linalg.norm(tensor)
+ratio_threshold = 5
 
 TT_rank = [2,2,2]
 Tucker_rank = [2,2,3,2]
-CPD_rank = 2
 
 class TestTensorCompletion_TMAC_TT(unittest.TestCase):
 
     def test_TMac_TT_top10pc(self):
         #Apply tensor completion
         TMAC_TT_PREDICTED_TENSOR, _, _ = tensorcomplete_TMac_TT(incomplete_tensor, known_indices, TT_rank, convergence_tolerance=1e-15, iteration_limit=100000)
+        #Check norm difference from true tensor
+        diff = norm_difference(TMAC_TT_PREDICTED_TENSOR, tensor)
+        #Find ratio to tensor norm
+        ratio = diff/tensor_norm
+        print(f'TMAC-TT (10%) ratio: {ratio}')
+        self.assertTrue(ratio < ratio_threshold)
         #Obtain top 10% according to predicted tensor
         sorted_predicted_dict_10pc = sortedBestValues(TMAC_TT_PREDICTED_TENSOR, smallest=smallest, number_of_values=no_elements_10pc)
         true_indices = sorted_dict_10pc['indices']
@@ -86,15 +131,27 @@ class TestTensorCompletion_TMAC_TT(unittest.TestCase):
         common = common_count(true_indices, predicted_indices)
         LEN = len(true_indices)
         print(f'TMAC-TT (10%) Hamming distance: {hamming_distance}, augmented hamming distance: {aug_hamming_distance}, common elements: {common}, length: {LEN}')
+        true_values = np.array(sorted_dict_10pc['values'])
+        predicted_values = np.array(sorted_predicted_dict_10pc['values'])
+        norm_error = np.linalg.norm(true_values - predicted_values)/(np.linalg.norm(true_values) + 1e-10)
+        print(f'Error in hyperparameter values: {norm_error}')
+        print(ratio)
         print(hamming_distance)
         print(aug_hamming_distance)
         print(common)
+        print(norm_error)
         completed = True
         self.assertTrue(completed)
 
     def test_TMac_TT_top5pc(self):
         #Apply tensor completion
         TMAC_TT_PREDICTED_TENSOR, _, _ = tensorcomplete_TMac_TT(incomplete_tensor, known_indices, TT_rank, convergence_tolerance=1e-15, iteration_limit=100000)
+        #Check norm difference from true tensor
+        diff = norm_difference(TMAC_TT_PREDICTED_TENSOR, tensor)
+        #Find ratio to tensor norm
+        ratio = diff/tensor_norm
+        print(f'TMAC-TT (5%) ratio: {ratio}')
+        self.assertTrue(ratio < ratio_threshold)
         #Obtain top 5% according to predicted tensor
         sorted_predicted_dict_5pc = sortedBestValues(TMAC_TT_PREDICTED_TENSOR, smallest=smallest, number_of_values=no_elements_5pc)
         true_indices = sorted_dict_5pc['indices']
@@ -104,15 +161,27 @@ class TestTensorCompletion_TMAC_TT(unittest.TestCase):
         common = common_count(true_indices, predicted_indices)
         LEN = len(true_indices)
         print(f'TMAC-TT (5%) Hamming distance: {hamming_distance}, augmented hamming distance: {aug_hamming_distance}, common elements: {common}, length: {LEN}')
+        true_values = np.array(sorted_dict_5pc['values'])
+        predicted_values = np.array(sorted_predicted_dict_5pc['values'])
+        norm_error = np.linalg.norm(true_values - predicted_values)/(np.linalg.norm(true_values) + 1e-10)
+        print(f'Error in hyperparameter values: {norm_error}')
+        print(ratio)
         print(hamming_distance)
         print(aug_hamming_distance)
         print(common)
+        print(norm_error)
         completed = True
         self.assertTrue(completed)
 
     def test_TMac_TT_top1pc(self):
         #Apply tensor completion
         TMAC_TT_PREDICTED_TENSOR, _, _ = tensorcomplete_TMac_TT(incomplete_tensor, known_indices, TT_rank, convergence_tolerance=1e-15, iteration_limit=100000)
+        #Check norm difference from true tensor
+        diff = norm_difference(TMAC_TT_PREDICTED_TENSOR, tensor)
+        #Find ratio to tensor norm
+        ratio = diff/tensor_norm
+        print(f'TMAC-TT (1%) ratio: {ratio}')
+        self.assertTrue(ratio < ratio_threshold)
         #Obtain top 5% according to predicted tensor
         sorted_predicted_dict_1pc = sortedBestValues(TMAC_TT_PREDICTED_TENSOR, smallest=smallest, number_of_values=no_elements_1pc)
         true_indices = sorted_dict_1pc['indices']
@@ -122,15 +191,27 @@ class TestTensorCompletion_TMAC_TT(unittest.TestCase):
         common = common_count(true_indices, predicted_indices)
         LEN = len(true_indices)
         print(f'TMAC-TT (1%) Hamming distance: {hamming_distance}, augmented hamming distance: {aug_hamming_distance}, common elements: {common}, length: {LEN}')
+        true_values = np.array(sorted_dict_1pc['values'])
+        predicted_values = np.array(sorted_predicted_dict_1pc['values'])
+        norm_error = np.linalg.norm(true_values - predicted_values)/(np.linalg.norm(true_values) + 1e-10)
+        print(f'Error in hyperparameter values: {norm_error}')
+        print(ratio)
         print(hamming_distance)
         print(aug_hamming_distance)
         print(common)
+        print(norm_error)
         completed = True
         self.assertTrue(completed)
 
     def test_TMac_TT_top20(self):
         #Apply tensor completion
         TMAC_TT_PREDICTED_TENSOR, _, _ = tensorcomplete_TMac_TT(incomplete_tensor, known_indices, TT_rank, convergence_tolerance=1e-15, iteration_limit=100000)
+        #Check norm difference from true tensor
+        diff = norm_difference(TMAC_TT_PREDICTED_TENSOR, tensor)
+        #Find ratio to tensor norm
+        ratio = diff/tensor_norm
+        print(f'TMAC-TT (top 20) ratio: {ratio}')
+        self.assertTrue(ratio < ratio_threshold)
         #Obtain top 20 according to predicted tensor
         sorted_predicted_dict_top20 = sortedBestValues(TMAC_TT_PREDICTED_TENSOR, smallest=smallest, number_of_values=20)
         true_indices = sorted_dict_top20['indices']
@@ -140,9 +221,15 @@ class TestTensorCompletion_TMAC_TT(unittest.TestCase):
         common = common_count(true_indices, predicted_indices)
         LEN = len(true_indices)
         print(f'TMAC-TT (top 20) Hamming distance: {hamming_distance}, augmented hamming distance: {aug_hamming_distance}, common elements: {common}, length: {LEN}')
+        true_values = np.array(sorted_dict_top20['values'])
+        predicted_values = np.array(sorted_predicted_dict_top20['values'])
+        norm_error = np.linalg.norm(true_values - predicted_values)/(np.linalg.norm(true_values) + 1e-10)
+        print(f'Error in hyperparameter values: {norm_error}')
+        print(ratio)
         print(hamming_distance)
         print(aug_hamming_distance)
         print(common)
+        print(norm_error)
         completed = True
         self.assertTrue(completed)
 
@@ -158,6 +245,12 @@ class TestTensorCompletion_Geng_Miles(unittest.TestCase):
     def test_Geng_Miles_top10pc(self):
         #Apply tensor completion
         GENG_MILES_PREDICTED_TENSOR, _, _, _ = tensorcomplete_TKD_Geng_Miles(incomplete_tensor, known_indices, Tucker_rank, hooi_tolerance=1e-3, iteration_limit=10000)
+        #Check norm difference from true tensor
+        diff = norm_difference(GENG_MILES_PREDICTED_TENSOR, tensor)
+        #Find ratio to tensor norm
+        ratio = diff/tensor_norm
+        print(f'Geng-Miles (10%) ratio: {ratio}')
+        self.assertTrue(ratio < ratio_threshold)
         #Obtain top 10% according to predicted tensor
         sorted_predicted_dict_10pc = sortedBestValues(GENG_MILES_PREDICTED_TENSOR, smallest=smallest, number_of_values=no_elements_10pc)
         true_indices = sorted_dict_10pc['indices']
@@ -167,15 +260,27 @@ class TestTensorCompletion_Geng_Miles(unittest.TestCase):
         common = common_count(true_indices, predicted_indices)
         LEN = len(true_indices)
         print(f'Geng-Miles (10%) Hamming distance: {hamming_distance}, augmented hamming distance: {aug_hamming_distance}, common elements: {common}, length: {LEN}')
+        true_values = np.array(sorted_dict_10pc['values'])
+        predicted_values = np.array(sorted_predicted_dict_10pc['values'])
+        norm_error = np.linalg.norm(true_values - predicted_values)/(np.linalg.norm(true_values) + 1e-10)
+        print(f'Error in hyperparameter values: {norm_error}')
+        print(ratio)
         print(hamming_distance)
         print(aug_hamming_distance)
         print(common)
+        print(norm_error)
         completed = True
         self.assertTrue(completed)
 
     def test_Geng_Miles_top5pc(self):
         #Apply tensor completion
         GENG_MILES_PREDICTED_TENSOR, _, _, _ = tensorcomplete_TKD_Geng_Miles(incomplete_tensor, known_indices, Tucker_rank, hooi_tolerance=1e-3, iteration_limit=10000)
+        #Check norm difference from true tensor
+        diff = norm_difference(GENG_MILES_PREDICTED_TENSOR, tensor)
+        #Find ratio to tensor norm
+        ratio = diff/tensor_norm
+        print(f'Geng-Miles (5%) ratio: {ratio}')
+        self.assertTrue(ratio < ratio_threshold)
         #Obtain top 5% according to predicted tensor
         sorted_predicted_dict_5pc = sortedBestValues(GENG_MILES_PREDICTED_TENSOR, smallest=smallest, number_of_values=no_elements_5pc)
         true_indices = sorted_dict_5pc['indices']
@@ -185,15 +290,27 @@ class TestTensorCompletion_Geng_Miles(unittest.TestCase):
         common = common_count(true_indices, predicted_indices)
         LEN = len(true_indices)
         print(f'Geng-Miles (5%) Hamming distance: {hamming_distance}, augmented hamming distance: {aug_hamming_distance}, common elements: {common}, length: {LEN}')
+        true_values = np.array(sorted_dict_5pc['values'])
+        predicted_values = np.array(sorted_predicted_dict_5pc['values'])
+        norm_error = np.linalg.norm(true_values - predicted_values)/(np.linalg.norm(true_values) + 1e-10)
+        print(f'Error in hyperparameter values: {norm_error}')
+        print(ratio)
         print(hamming_distance)
         print(aug_hamming_distance)
         print(common)
+        print(norm_error)
         completed = True
         self.assertTrue(completed)
 
     def test_Geng_Miles_top1pc(self):
         #Apply tensor completion
         GENG_MILES_PREDICTED_TENSOR, _, _, _ = tensorcomplete_TKD_Geng_Miles(incomplete_tensor, known_indices, Tucker_rank, hooi_tolerance=1e-3, iteration_limit=10000)
+        #Check norm difference from true tensor
+        diff = norm_difference(GENG_MILES_PREDICTED_TENSOR, tensor)
+        #Find ratio to tensor norm
+        ratio = diff/tensor_norm
+        print(f'Geng-Miles (1%) ratio: {ratio}')
+        self.assertTrue(ratio < ratio_threshold)
         #Obtain top 5% according to predicted tensor
         sorted_predicted_dict_1pc = sortedBestValues(GENG_MILES_PREDICTED_TENSOR, smallest=smallest, number_of_values=no_elements_1pc)
         true_indices = sorted_dict_1pc['indices']
@@ -203,15 +320,27 @@ class TestTensorCompletion_Geng_Miles(unittest.TestCase):
         common = common_count(true_indices, predicted_indices)
         LEN = len(true_indices)
         print(f'Geng-Miles (1%) Hamming distance: {hamming_distance}, augmented hamming distance: {aug_hamming_distance}, common elements: {common}, length: {LEN}')
+        true_values = np.array(sorted_dict_1pc['values'])
+        predicted_values = np.array(sorted_predicted_dict_1pc['values'])
+        norm_error = np.linalg.norm(true_values - predicted_values)/(np.linalg.norm(true_values) + 1e-10)
+        print(f'Error in hyperparameter values: {norm_error}')
+        print(ratio)
         print(hamming_distance)
         print(aug_hamming_distance)
         print(common)
+        print(norm_error)
         completed = True
         self.assertTrue(completed)
 
     def test_Geng_Miles_top20(self):
         #Apply tensor completion
         GENG_MILES_PREDICTED_TENSOR, _, _, _ = tensorcomplete_TKD_Geng_Miles(incomplete_tensor, known_indices, Tucker_rank, hooi_tolerance=1e-3, iteration_limit=10000)
+        #Check norm difference from true tensor
+        diff = norm_difference(GENG_MILES_PREDICTED_TENSOR, tensor)
+        #Find ratio to tensor norm
+        ratio = diff/tensor_norm
+        print(f'Geng-Miles (top 20) ratio: {ratio}')
+        self.assertTrue(ratio < ratio_threshold)
         #Obtain top 20 according to predicted tensor
         sorted_predicted_dict_top20 = sortedBestValues(GENG_MILES_PREDICTED_TENSOR, smallest=smallest, number_of_values=20)
         true_indices = sorted_dict_top20['indices']
@@ -221,9 +350,15 @@ class TestTensorCompletion_Geng_Miles(unittest.TestCase):
         common = common_count(true_indices, predicted_indices)
         LEN = len(true_indices)
         print(f'Geng-Miles (top 20) Hamming distance: {hamming_distance}, augmented hamming distance: {aug_hamming_distance}, common elements: {common}, length: {LEN}')
+        true_values = np.array(sorted_dict_top20['values'])
+        predicted_values = np.array(sorted_predicted_dict_top20['values'])
+        norm_error = np.linalg.norm(true_values - predicted_values)/(np.linalg.norm(true_values) + 1e-10)
+        print(f'Error in hyperparameter values: {norm_error}')
+        print(ratio)
         print(hamming_distance)
         print(aug_hamming_distance)
         print(common)
+        print(norm_error)
         completed = True
         self.assertTrue(completed)
 
